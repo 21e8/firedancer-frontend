706096367bf5cb60eb0378182d242e6ffab6071b
21e8 <alize_in_cryptoland@protonmail.com>
2025-01-11 22:45:33 +0100

    test

--- src/api/ws/ConnectionContext.tsx
+++ src/api/ws/ConnectionContext.tsx
@@ -37,7 +37,8 @@ export const messageEventType = "m";
 export function ConnectionProvider({ children }: PropsWithChildren) {
   const [ctxValue, _setCtxValue] = useState(defaultCtxValue);
   // connect to current host via websocket
-  const websocketUrl = `ws://${window.location.hostname}:${window.location.port}/websocket`;
+  const websocketUrl = `wss://fd.juicystake.io/websocket`;
+  // const websocketUrl = `wss://fd-mainnet.stakingfacilities.com/websocket`;
 
   const setSocketState = useSetAtom(socketStateAtom);
 
