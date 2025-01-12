ada511b6796e591fd2b86c2e9a71836e4d06dccb
21e8 <alize_in_cryptoland@protonmail.com>
2025-01-12 17:37:42 +0100

    fixes

--- src/api/ws/atoms.ts
+++ src/api/ws/atoms.ts
@@ -1,19 +1,39 @@
 import { atom } from "jotai";
 import { SocketState } from "./types";
+const isProd = import.meta.env.PROD;
 
 export const socketStateAtom = atom<SocketState>(SocketState.Disconnected);
 
-export const websocketUrlAtom = atom<string>(
-  `wss://fd.juicystake.io/websocket`
-);
-
-export const knownSocketUrlsAtom = atom<{ name: string; url: string }[]>([
-  {
-    name: "JuicyStake",
-    url: `wss://fd.juicystake.io/websocket`,
-  },
+const KNOWN_SOCKET_URLS = [
   {
     name: "StakingFacilities",
     url: `wss://fd-mainnet.stakingfacilities.com/websocket`,
   },
-]);
+  {
+    name: "JuicyStake",
+    url: `wss://fd.juicystake.io/websocket`,
+  },
+];
+
+export const websocketUrlAtom = atom<string>(
+  isProd
+    ? KNOWN_SOCKET_URLS[0].url
+    : `ws://${window.location.hostname}:${window.location.port}/websocket`
+);
+
+export const knownSocketUrlsAtom = atom<{ name: string; url: string }[]>(
+  [
+    {
+      name: "localhost",
+      url:
+        (import.meta.env.VITE_DEFAULT_WS_URL as string) ||
+        `ws://${window.location.hostname}:${window.location.port}/websocket`,
+    },
+    ...KNOWN_SOCKET_URLS,
+  ].filter((url) => {
+    if (isProd && url.name === "localhost") {
+      return false;
+    }
+    return true;
+  })
+);
